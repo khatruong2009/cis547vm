16a14f97d839a3ff451fa8deb969e31a6b1edc6f
Kha Truong <64438356+khatruong2009@users.noreply.github.com>
2022-10-26 15:34:34 -0400

    failed attempt

--- lab6/src/DivZeroAnalysis.cpp
+++ lab6/src/DivZeroAnalysis.cpp
@@ -1,44 +1,60 @@
 #include "DivZeroAnalysis.h"
 #include "Utils.h"
+#include <iostream>
 
-namespace dataflow {
+namespace dataflow
+{
 
-//===----------------------------------------------------------------------===//
-// DivZero Analysis Implementation
-//===----------------------------------------------------------------------===//
+  //===----------------------------------------------------------------------===//
+  // DivZero Analysis Implementation
+  //===----------------------------------------------------------------------===//
 
-/**
- * PART 1
- * 1. Implement "check" that checks if a given instruction is erroneous or not.
- * 2. Implement "transfer" that computes the semantics of each instruction.
- *    This means that you have to complete "eval" function, too.
- *
- * PART 2
- * 1. Implement "doAnalysis" that stores your results in "InMap" and "OutMap".
- * 2. Implement "flowIn" that joins the memory set of all incoming flows.
- * 3. Implement "flowOut" that flows the memory set to all outgoing flows.
- * 4. Implement "join" to union two Memory objects, accounting for Domain value.
- * 5. Implement "equal" to compare two Memory objects.
- */
-
-bool DivZeroAnalysis::check(Instruction *Inst) {
   /**
-   * TODO: Write your code to check if Inst can cause a division by zero.
+   * PART 1
+   * 1. Implement "check" that checks if a given instruction is erroneous or not.
+   * 2. Implement "transfer" that computes the semantics of each instruction.
+   *    This means that you have to complete "eval" function, too.
    *
-   * Inst can cause a division by zero if:
-   *   Inst is a signed or unsigned division instruction and,
-   *   The divisor is either Zero or MaybeZero.
-   *
-   * Hint: getOrExtract function may be useful to simplify your code.
+   * PART 2
+   * 1. Implement "doAnalysis" that stores your results in "InMap" and "OutMap".
+   * 2. Implement "flowIn" that joins the memory set of all incoming flows.
+   * 3. Implement "flowOut" that flows the memory set to all outgoing flows.
+   * 4. Implement "join" to union two Memory objects, accounting for Domain value.
+   * 5. Implement "equal" to compare two Memory objects.
    */
-  return false;
+
+  bool DivZeroAnalysis::check(Instruction *Inst)
+  {
+    /**
+     * TODO: Write your code to check if Inst can cause a division by zero.
+     *
+     * Inst can cause a division by zero if:
+     *   Inst is a signed or unsigned division instruction and,
+     *   The divisor is either Zero or MaybeZero.
+     *
+     * Hint: getOrExtract function may be useful to simplify your code.
+     */
+
+    // return true if divisor is either zero or maybe zero
+
+    if (auto *binary = dyn_cast<BinaryOperator>(Inst))
+    {
+      auto inMemory = InMap[Inst];
+      std::cout << inMemory[Inst->getName()];
+      return true;
+    }
+
+    return false;
+  }
 }
 
-bool DivZeroAnalysis::runOnFunction(Function &F) {
+bool DivZeroAnalysis::runOnFunction(Function &F)
+{
   outs() << "Running " << getAnalysisName() << " on " << F.getName() << "\n";
 
   // Initializing InMap and OutMap.
-  for (inst_iterator Iter = inst_begin(F), End = inst_end(F); Iter != End; ++Iter) {
+  for (inst_iterator Iter = inst_begin(F), End = inst_end(F); Iter != End; ++Iter)
+  {
     auto Inst = &(*Iter);
     InMap[Inst] = new Memory;
     OutMap[Inst] = new Memory;
@@ -48,7 +64,8 @@ bool DivZeroAnalysis::runOnFunction(Function &F) {
   doAnalysis(F);
 
   // Check each instruction in function F for potential divide-by-zero error.
-  for (inst_iterator Iter = inst_begin(F), End = inst_end(F); Iter != End; ++Iter) {
+  for (inst_iterator Iter = inst_begin(F), End = inst_end(F); Iter != End; ++Iter)
+  {
     auto Inst = &(*Iter);
     if (check(Inst))
       ErrorInsts.insert(Inst);
@@ -56,11 +73,13 @@ bool DivZeroAnalysis::runOnFunction(Function &F) {
 
   printMap(F, InMap, OutMap);
   outs() << "Potential Instructions by " << getAnalysisName() << ": \n";
-  for (auto Inst : ErrorInsts) {
+  for (auto Inst : ErrorInsts)
+  {
     outs() << *Inst << "\n";
   }
 
-  for (auto Iter = inst_begin(F), End = inst_end(F); Iter != End; ++Iter) {
+  for (auto Iter = inst_begin(F), End = inst_end(F); Iter != End; ++Iter)
+  {
     delete InMap[&(*Iter)];
     delete OutMap[&(*Iter)];
   }
@@ -68,6 +87,5 @@ bool DivZeroAnalysis::runOnFunction(Function &F) {
 }
 
 char DivZeroAnalysis::ID = 1;
-static RegisterPass<DivZeroAnalysis> X("DivZero", "Divide-by-zero Analysis",
-                                       false, false);
-} // namespace dataflow
+static RegisterPass<DivZeroAnalysis> X("DivZero", "Divide-by-zero Analysis", false, false);
+// namespace dataflow
